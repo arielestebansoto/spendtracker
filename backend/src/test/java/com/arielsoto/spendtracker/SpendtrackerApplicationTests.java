12609e9e19b606f4947d4ea592ae7664bfd3c29b
Ariel Esteban Soto <72299420+arielestebansoto@users.noreply.github.com>
2026-05-09 10:40:40 -0300

    feat(app): First setup

---
+++ backend/src/test/java/com/arielsoto/spendtracker/SpendtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arielsoto.spendtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
