b54ba16f83d8eab8a11b3e532fd3eec82aefe30a
Ariel Esteban Soto <72299420+arielestebansoto@users.noreply.github.com>
2026-05-11 22:04:06 -0300

    feat(3-app): Manage flyway in boot application

--- backend/src/main/java/com/arielsoto/spendtracker/SpendtrackerApplication.java
+++ backend/src/main/java/com/arielsoto/spendtracker/SpendtrackerApplication.java
@@ -1,7 +1,10 @@
 package com.arielsoto.spendtracker;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpendtrackerApplication {
@@ -10,4 +13,11 @@ public class SpendtrackerApplication {
 		SpringApplication.run(SpendtrackerApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner flywayClean(Flyway flyway) {
+		return args -> {
+			// flyway.clean();
+			// flyway.migrate();
+		};
+	}
 }
