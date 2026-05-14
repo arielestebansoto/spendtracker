dffbd5ac1caae6aba4132d2349c77a700877e8c4
Ariel Esteban Soto <72299420+arielestebansoto@users.noreply.github.com>
2026-05-13 23:16:15 -0300

    feat(6-security): Implement custom service for load user

--- backend/src/main/java/com/arielsoto/spendtracker/security/SecurityConfig.java
+++ backend/src/main/java/com/arielsoto/spendtracker/security/SecurityConfig.java
@@ -2,25 +2,33 @@ package com.arielsoto.spendtracker.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
     
+    private final CustomOAuth2UserService customOAuth2UserService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/").permitAll()
+                .requestMatchers("/", "/error").permitAll()
                 .anyRequest().authenticated()
             )
-            .oauth2Login(Customizer.withDefaults())
+            .oauth2Login(oauth -> oauth
+                .userInfoEndpoint(userInfo ->
+                    userInfo.userService(customOAuth2UserService)
+                )
+            )
             .build();
     }
 }
