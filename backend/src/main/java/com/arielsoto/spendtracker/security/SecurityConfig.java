66638717b3d7ebb575e3d901a3ad38955fc82781
Ariel Esteban Soto <72299420+arielestebansoto@users.noreply.github.com>
2026-05-23 10:11:57 -0300

    feat(12-security): Basic cors config

--- backend/src/main/java/com/arielsoto/spendtracker/security/SecurityConfig.java
+++ backend/src/main/java/com/arielsoto/spendtracker/security/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.arielsoto.spendtracker.security;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -18,20 +26,57 @@ public class SecurityConfig {
 
     private final CustomOidcUserService customOidcUserService;
 
+    @Value("#{'${app.cors.allowed-origins}'.split(',')}")
+    private List<String> allowedOrigins;
+
+    @Value("#{'${app.cors.allowed-methods}'.split(',')}")
+    private List<String> allowedMethods;
+
+    @Value("${app.oauth2.default-success-url}")
+    private String oauth2DefaultSuccessUrl;
+
+    @Value("${app.logout.logout-url}")
+    private String logoutUrl;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
             .csrf(AbstractHttpConfigurer::disable)
+
+            .cors(Customizer.withDefaults())
+
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/error").permitAll()
+                .requestMatchers(
+                    "/", 
+                    "/error"
+                ).permitAll()
                 .anyRequest().authenticated()
             )
+
             .oauth2Login(oauth -> oauth
                 .userInfoEndpoint(userInfo -> userInfo
                     .userService(customOAuth2UserService)
                     .oidcUserService(customOidcUserService)
                 )
             )
+
             .build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.setAllowedOrigins(allowedOrigins);
+
+        config.setAllowedMethods(allowedMethods);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
 }
