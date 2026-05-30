162b59937a0d063b8d12c81b6794635a3858cb4c
Ariel Esteban Soto <72299420+arielestebansoto@users.noreply.github.com>
2026-05-29 23:21:50 -0300

    feat(12-security): Create endpoint for return csrf

---
+++ backend/src/main/java/com/arielsoto/spendtracker/security/CsrfController.java
@@ -0,0 +1,17 @@
+package com.arielsoto.spendtracker.security;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/security")
+public class CsrfController {
+
+    @GetMapping("/csrf")
+    public CsrfToken csrf(CsrfToken token) {
+        return token;
+    }
+
+}
