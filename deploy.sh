d33699d429a5540a92db79359750a4d33800b6f2
Ariel Esteban Soto <72299420+arielestebansoto@users.noreply.github.com>
2026-08-06 17:41:23 -0300

    fix(43-deploy): Use docker compose prod file (#44)

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,20 @@ set -e
 
 cd /home/ec2-user/dev/spendtracker
 
+echo "Pulling main repository"
+git pull origin main
+
 echo "Clean cache images..."
 docker builder prune -af
 docker image prune -f
 
+COMPOSE="-f docker-compose.yml -f docker-compose.prod.yml"
+
 echo "Pulling latest images..."
-docker compose pull
+docker compose $COMPOSE pull
 
 echo "Deploying..."
-docker compose up -d --remove-orphans
+docker compose $COMPOSE up -d --remove-orphans
 
 echo "Cleaning old images..."
 docker image prune -f
